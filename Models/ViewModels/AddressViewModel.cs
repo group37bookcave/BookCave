97a7f1b34b1f7ad723157f52c89d7e13d214b301
Ívar Loftsson <ivar15@ru.is>
2018-05-05 11:47:47 +0000

    ViewModels

---
+++ Models/ViewModels/AddressViewModel.cs
@@ -0,0 +1,10 @@
+namespace BookCave.Models.ViewModels
+{
+    public class AddressViewModel
+    {
+        public string Street { get; set; }
+        public int Zipcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
