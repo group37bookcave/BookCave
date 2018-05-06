6711d58e8960ae2eb6f3d99860c2099b50729084
andreas <andreas.estensson@gmail.com>
2018-05-06 12:55:00 +0000

    resolved conflicts

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BookCave.Models;
+using BookCave.Models.ViewModels;
 
 namespace BookCave.Controllers
 {
@@ -38,43 +38,11 @@ namespace BookCave.Controllers
             return View();
         }
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
-}
-=======
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using BookCave.Models;
-using BookCave.Models.ViewModels;
-
-namespace BookCave.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+        ViewData["Message"] = "Your contact page.";
+        
+        return View();
         }
 
         public IActionResult Error()
@@ -83,4 +51,3 @@ namespace BookCave.Controllers
         }
     }
 }
->>>>>>> develop
