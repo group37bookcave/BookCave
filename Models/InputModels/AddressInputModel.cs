cd6ee7baca60116cb0b60894ced098e476be3e62
Birgitta <birgittab15@ru.is>
2018-05-09 22:20:51 +0000

    payment

--- Models/InputModels/AddressInputModel.cs
+++ Models/InputModels/AddressInputModel.cs
@@ -1,13 +1,18 @@
-﻿namespace BookCave.Repositories
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BookCave.Repositories
 {
     public class AddressInputModel
     {
+        [Required(ErrorMessage="Please enter street.")]
         public string Street { get; set; }
 
+        [Required(ErrorMessage="Please enter ZipCode.")]
         public string Zipcode { get; set; }
-
+        [Required(ErrorMessage="Please enter city.")]   
         public string City { get; set; }
-
+        [Required(ErrorMessage="Please select country.")]
         public int CountryId { get; set; }
     }
 }
