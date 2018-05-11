75960ba9638bb172d04dd0d243888988ebd4d233
Dagný Ósk <dagnyoskg@gmail.com>
2018-05-11 13:31:44 +0000

    date added

--- Models/ViewModels/OrderViewModel.cs
+++ Models/ViewModels/OrderViewModel.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+
 namespace BookCave.Models.ViewModels
 {
     public class OrderViewModel 
     {
         public int OrderId { get; set; }
+        
         public int CustomerId { get; set; }
+        
+        public DateTime OrderDate { get; set; }
+        
         public List<ItemOrderViewModel> Items { get; set; }
     }
 }
